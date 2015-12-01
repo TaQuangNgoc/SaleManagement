0a953cbd502a701c1900b3a6fe420dd6af75cd2b
Thanh <phamhathanh130394@gmail.com>
2015-12-01 19:54:16 +0700

    restructure

---
+++ TestData/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestData
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // test goes here
+        }
+    }
+}
